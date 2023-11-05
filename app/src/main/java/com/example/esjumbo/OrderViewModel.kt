4ff513de2e2f6fde2583fa471aa60553dd0f282a
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-11-05 17:56:47 +0700

    Initial commit not complete

---
+++ app/src/main/java/com/example/esjumbo/OrderViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.esjumbo
+
+import android.icu.text.NumberFormat
+import androidx.lifecycle.ViewModel
+import com.example.esjumbo.data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+private  const val Harga_per_cup = 3000
+
+class OrderViewModel : ViewModel() {
+
+    private val _stateUI = MutableStateFlow(OrderUIState())
+    val stateUi: StateFlow<OrderUIState> = _stateUI.asStateFlow()
+
+
+    fun setJumlah (jmlEsjumbo:Int){
+        _stateUI.update { stateSaatIni -> stateSaatIni.copy(
+            jumlah = jmlEsjumbo,
+            harga = hitungHarga(jumlah = jmlEsjumbo)
+        ) }
+    }
+
+    fun setRasa(rasaPilihan: String){
+        _stateUI.update { stateSaatIni -> stateSaatIni.copy(rasa = rasaPilihan) }
+    }
+    fun resetOrder(){
+        _stateUI.value = OrderUIState()
+    }
+
+    private fun hitungHarga( jumlah:Int = _stateUI.value.jumlah,
+                             ): String{
+        val kalkulasiHarga = jumlah * Harga_per_cup
+        return NumberFormat.getNumberInstance().format(kalkulasiHarga)
+    }
+}
---
+++ app/src/main/java/com/example/esjumbo/halamandua.kt
@@ -0,0 +1,82 @@
+package com.example.dumdumteaapp
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.example.esjumbo.R
+import com.example.esjumbo.data.OrderUIState
+import com.example.esjumbo.ui.theme.komponen.FormatlabelHarga
+
+@SuppressLint("ResourceType")
+@Composable
+fun HalamanDua(
+    orderUIState: OrderUIState,
+    onCancelButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    val items = listOf(
+        Pair(stringArrayResource(R.string.quantity), orderUIState.jumlah),
+        Pair(stringResource(R.string.flavor), orderUIState.rasa)
+
+    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            items.forEach{items ->
+                Column {
+                    Text(items.first.toString().uppercase())
+                    Text(text = items.second.toString(),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+            FormatlabelHarga(subtotal = orderUIState.harga,
+                modifier= Modifier.align(Alignment.End)
+            )
+        }
+        Row (
+            modifier = Modifier
+                .weight(1f, false)
+                .padding(dimensionResource(R.dimen.padding_medium))
+        ){
+            Column(
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+            ) {
+                Button(modifier = Modifier.fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(stringResource(R.string.send))
+                }
+                OutlinedButton(modifier = Modifier.fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(stringResource(R.string.cansel))
+                }
+            }
+        }
+    }
+}
